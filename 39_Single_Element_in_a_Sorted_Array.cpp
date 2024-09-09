abfd309fef7118fc8e15fb9540e3d3146989a0f9
aniketkumar7 <aniketkumar639597@gmail.com>
2024-09-09 16:54:47 +0530

    Solve Problem 39

---
+++ 39_Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,83 @@
+// Question: Single Element in a Sorted Array
+// Question Link: https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+
+// Problem Statement:
+// You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+// Example 1:
+
+// Input: [1,1,2,3,3,4,4,8,8]
+// Output: 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Brute Force Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+int singleNonDuplicate1(vector<int> &nums)
+{
+    int n = nums.size();
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans ^= nums[i];
+    }
+    return ans;
+}
+
+
+// Brute Force Approach:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+int singleNonDuplicate2(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i += 2)
+    {
+        if (i == nums.size() - 1 || nums[i] != nums[i + 1])
+        {
+            return nums[i];
+        }
+    }
+    return -1;
+}
+
+// Optimal Approach:
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+int singleNonDuplicate3(vector<int> &nums)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (mid % 2 == 1)
+            mid--;
+        if (nums[mid] == nums[mid + 1])
+            low = mid + 2;
+        else
+            high = mid - 1;
+    }
+    return nums[low];
+}
+
+
+int main()
+{
+    vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+    cout << "Input: " << endl;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+    }
+
+    cout << "\nOutput: " << endl;
+    cout << singleNonDuplicate1(nums) << endl;
+    cout << singleNonDuplicate2(nums) << endl;
+    cout << singleNonDuplicate3(nums) << endl;
+    return 0;
+}
