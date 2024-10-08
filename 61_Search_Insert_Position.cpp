dbbd66feb69bcebb490467317312226f1bfb28c6
aniketkumar7 <aniketkumar639597@gmail.com>
2024-10-08 20:49:45 +0530

    Solve problem 61

---
+++ 61_Search_Insert_Position.cpp
@@ -0,0 +1,44 @@
+// Question: Search Insert Position
+// Question Link: https://leetcode.com/problems/search-insert-position/
+
+// Problem Statement:
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Code:
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target) {
+            return mid;
+        } else if(nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 5, 6};
+    cout << "Array: ";
+    for(int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    int target = 5;
+    cout << "\nTarget: " << target << endl;
+    cout << "Output: ";
+    cout << searchInsert(nums, target) << endl;
+    return 0;
+}
