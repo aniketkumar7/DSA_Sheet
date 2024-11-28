2d3b1353c2b2c0ac973a3541768ab849e340f871
aniketkumar7 <aniketkumar639597@gmail.com>
2024-11-28 06:33:34 +0530

    Add an approach

--- 4_TrappingRainWater.cpp
+++ 4_TrappingRainWater.cpp
@@ -77,6 +77,43 @@ int WaterTrapped(vector<int>& height)
 }
 
 
+// Optimized approach
+
+int WaterTrapped(vector<int>& height)
+{
+    int n = height.size();
+
+    if(n == 0)
+        return 0;
+
+    int left = 0, right = n-1;
+    int leftMax = 0, rightMax = 0;
+    int trappedWater = 0;
+
+    while(left < right)
+    {
+        if(height[left] < height[right])
+        {
+            if(height[left] >= leftMax)
+                leftMax = height[left];
+            else
+                trappedWater += leftMax - height[left];
+            left++;
+        }
+        else
+        {
+            if(height[right] >= rightMax)
+                rightMax = height[right];
+            else
+                trappedWater += rightMax - height[right];
+            right--;
+        }
+    }
+
+    return trappedWater;
+}
+
+
 int main()
 {
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
