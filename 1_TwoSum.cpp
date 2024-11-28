4c81e93c73fbbdfd84147cfd62d2e50e5e4c9f12
aniketkumar7 <aniketkumar639597@gmail.com>
2024-11-28 06:32:19 +0530

    Add one more approach

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -19,6 +19,42 @@
 #include<unordered_map>
 using namespace std;
 
+vector<int> twoSum(vector<int> &nums, int target){
+    vector<int> ans;
+    for(int i=0; i<nums.size(); i++){
+        for(int j=i+1; j<nums.size(); j++){
+            if(nums[i] + nums[j] == target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+    return ans;
+}
+
+
+// using two pointer approach
+vector<int> twoSum(vector<int> &nums, int target){
+    vector<int> ans;
+    int i=0, j=nums.size()-1;
+    while(i<j){
+        if(nums[i] + nums[j] == target){
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+        else if(nums[i] + nums[j] < target){
+            i++;
+        }
+        else{
+            j--;
+        }
+    }
+    return ans;
+}
+
+
 // function to find two numbers whose sum is equal to given number
 vector<int> twoSum(vector<int> &nums, int target){
     // create a map to store the numbers
